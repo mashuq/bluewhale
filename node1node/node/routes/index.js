66afdf6c41a7c27cf39e0befc677db63ebb5d20f
Mashuq Rahman <mashuq@gmail.com>
2019-03-30 16:39:13 +0600

    Changing node project

--- node1node/node/routes/index.js
+++ node1node/node/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var os = require("os");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({app: 'node express', host: os.hostname()});
 });
 
 module.exports = router;
