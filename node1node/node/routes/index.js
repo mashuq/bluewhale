bfa726d09fc74b52f41f972063a898e6f40e994b
Mashuq Rahman <mashuq@gmail.com>
2019-04-05 19:47:24 +0600

    Changes to the test apps for the network tests

--- node1node/node/routes/index.js
+++ node1node/node/routes/index.js
@@ -1,10 +1,33 @@
 var express = require('express');
 var router = express.Router();
 var os = require("os");
+const axios = require('axios');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({app: 'node express', host: os.hostname()});
+router.get('/data/', async function (req, res, next) {
+  res.json({
+    app: 'node express',
+    host: os.hostname(),
+  });
+});
+
+/* GET home page. */
+router.get('/', async function (req, res, next) {
+  try {
+    const spring = await axios.get('http://spring:8080/data/');
+    res.json({
+      app: 'node express',
+      host: os.hostname(),
+      spring: spring,
+    });
+  } catch (e) {
+    console.log(e);
+    res.json({
+      app: 'node express',
+      host: os.hostname(),
+      spring: null
+    });
+  }
 });
 
 module.exports = router;
--- node2spring/spring/src/main/java/com/mashuq/spring/controller/RestfulController.java
+++ node2spring/spring/src/main/java/com/mashuq/spring/controller/RestfulController.java
@@ -2,6 +2,7 @@ package com.mashuq.spring.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -13,7 +14,7 @@ public class RestfulController {
 
     private String hostName;
 
-    public RestfulController(){
+    public RestfulController() {
         try {
             InetAddress ip = InetAddress.getLocalHost();
             hostName = ip.getHostName();
@@ -28,6 +29,29 @@ public class RestfulController {
         Map map = new HashMap();
         map.put("app", "java spring");
         map.put("host", hostName);
+        map.put("node", getNodeData());
         return map;
+    }
+
+    @RequestMapping("/data/")
+    Map data() {
+        Map map = new HashMap();
+        map.put("response", "data from java spring");
+        map.put("host", hostName);
+        return map;
+    }
+
+
+    private static String getNodeData() {
+        final String uri = "http://node:8080/data/";
+
+        RestTemplate restTemplate = new RestTemplate();
+        String result = null;
+        try {
+            result = restTemplate.getForObject(uri, String.class);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return result;
     }
 }
