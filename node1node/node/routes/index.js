877935ac7665358d13750bd44be7578420e1f796
Mashuq Rahman <mashuq@gmail.com>
2019-04-08 21:13:34 +0600

    Changes for volume work

--- node1node/node/routes/index.js
+++ node1node/node/routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var os = require("os");
 const axios = require('axios');
+const { closeSync, openSync, readdirSync } = require('fs');
+const dataDir = './data/';
+const uuidv4 = require('uuid/v4');
 
 /* GET home page. */
 router.get('/data/', async function (req, res, next) {
@@ -13,21 +16,30 @@ router.get('/data/', async function (req, res, next) {
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
+  let uuid = uuidv4();
+  touch(uuid);
   try {
     const spring = await axios.get('http://spring:8080/data/');
     res.json({
       app: 'node express',
       host: os.hostname(),
       spring: spring,
+      number: number(dataDir),
     });
   } catch (e) {
-    console.log(e);
     res.json({
       app: 'node express',
       host: os.hostname(),
-      spring: null
+      spring: null,
+      number: number(dataDir),
     });
   }
 });
+
+const touch = filename => closeSync(openSync(dataDir + filename, 'w'));
+const number = dir => {
+  let files = readdirSync(dir);
+  return files.length;
+}
 
 module.exports = router;
